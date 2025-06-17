38dc5dd480dcaee896355b829f85f37c075fc66a
keegandragon <zenerith@gmail.com>
2025-06-17 17:00:49 -0400

    Alternative server setup and preview options

--- vite.config.ts
+++ vite.config.ts
@@ -8,17 +8,7 @@ export default defineConfig({
     host: '0.0.0.0',
     port: 5173,
     strictPort: false,
-    open: false,
-    cors: true,
-    hmr: {
-      port: 5174,
-      host: 'localhost'
-    }
-  },
-  preview: {
-    host: '0.0.0.0',
-    port: 4173,
-    strictPort: false
+    open: false
   },
   define: {
     global: 'globalThis',
